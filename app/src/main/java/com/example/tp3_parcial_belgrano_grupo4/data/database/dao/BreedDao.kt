b1187fc17cb12e3151f14ae2d7d509019fe4b58e
rosa2108 <rosannyguzman@gmail.com>
2023-11-02 13:36:19 -0300

    Room base structure

--- app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/database/dao/BreedDao.kt
+++ app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/database/dao/BreedDao.kt
@@ -4,15 +4,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.tp3_parcial_belgrano_grupo4.data.entities.RazaEntity
+import com.example.tp3_parcial_belgrano_grupo4.data.database.entities.BreedEntity
 
 @Dao
 interface BreedDao {
     // OBTENER TODAS LAS RAZAS
-    @Query("SELECT * FROM Raza_table")
-    suspend fun getAllRazas(): List<RazaEntity>
+    @Query("SELECT * FROM Breed_table")
+    suspend fun getAllBreeds(): List<BreedEntity>
 
     // INSERTAR TODAS LAS RAZAS - EN CASO DE EXISTIR UN CONFLICTO LO REEMPLAZA
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(razas: List<RazaEntity>)
+    suspend fun insertAll(breeds: List<BreedEntity>)
 }
---
+++ app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/database/dao/DogDao.kt
@@ -0,0 +1,16 @@
+package com.example.tp3_parcial_belgrano_grupo4.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.tp3_parcial_belgrano_grupo4.data.database.entities.DogEntity
+
+@Dao
+interface DogDao {
+    @Query("SELECT * FROM Dog_table")
+    suspend fun getAllDogs(): List<DogEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(dogs: List<DogEntity>)
+}
