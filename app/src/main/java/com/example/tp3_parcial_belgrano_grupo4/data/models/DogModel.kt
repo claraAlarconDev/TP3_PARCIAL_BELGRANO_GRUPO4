c2db678fab4b63f5a4c3cf6877a5c4b9bf23a580
Melisa <melisa.barboza@hotmail.com>
2023-11-03 17:40:50 -0300

    agrego service integrado a la api de perros usando retrofit

--- app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/models/DogModel.kt
+++ app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/models/DogModel.kt
@@ -5,11 +5,13 @@ import com.google.gson.annotations.SerializedName
 data class DogModel(
     @SerializedName("idDog") val idDog: Int,
     @SerializedName("name") val name: String,
-    @SerializedName("age")val age: Int,
+    @SerializedName("age") val age: Int,
     @SerializedName("sex") val sex: String,
     @SerializedName("description") val description: String,
     @SerializedName("observations") val observations: String,
     @SerializedName("adopted") val adopted: Boolean,
     @SerializedName("idOwner") val idOwner: Int,
-    @SerializedName("idBreed") val idBreed: Int
+    @SerializedName("idBreed") val idBreed: Int,
+    val location: String,
+    val weight: String
 )
