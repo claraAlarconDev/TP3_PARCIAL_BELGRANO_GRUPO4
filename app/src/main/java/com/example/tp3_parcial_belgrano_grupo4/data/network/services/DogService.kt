a6850fa09d5f1b011ee0c147203e1c5571e6ac2b
Clara V. Alarcon <claravictorialarcon@gmail.com>
2023-11-06 00:19:25 -0300

    refactor structure in network package

---
+++ app/src/main/java/com/example/tp3_parcial_belgrano_grupo4/data/network/services/DogService.kt
@@ -0,0 +1,35 @@
+package com.example.tp3_parcial_belgrano_grupo4.data.network.services
+
+import com.example.tp3_parcial_belgrano_grupo4.data.models.DogApiResponse
+import com.example.tp3_parcial_belgrano_grupo4.data.network.interfaces.DogsApi
+import retrofit2.Call
+import javax.inject.Inject
+
+//Ejemplo por si deseas tomar en cuenta
+//Clase Interactua con la api de perros
+//Clase inyectable (Dagger Hilt)
+class DogService @Inject constructor(
+    private val api: DogsApi
+) {
+
+    fun listAllBreeds(): Call<DogApiResponse<Map<String, List<String>>>> {
+        return api.listAllBreeds()
+    }
+
+    fun getRandomImage(): Call<DogApiResponse<String>> {
+        return api.getRandomImage()
+    }
+
+    fun getImagesByBreed(breed: String): Call<DogApiResponse<List<String>>> {
+        return api.getImagesByBreed(breed)
+    }
+
+    fun listAllSubBreeds(breed: String): Call<DogApiResponse<List<String>>> {
+        return api.listAllSubBreeds(breed)
+    }
+
+    fun getRandomImageByBreed(breed: String): Call<DogApiResponse<String>> {
+        return api.getRandomImageByBreed(breed)
+    }
+}
+
